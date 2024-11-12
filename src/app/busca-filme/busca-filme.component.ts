5f2be70c033e17baaf8dd5fae1ad810545b6da60
Malu <mariiamarqs@gmail.com>
2024-11-12 13:55:17 -0300

    inicio busca-filme

---
+++ src/app/busca-filme/busca-filme.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-busca-filme',
+  templateUrl: './busca-filme.component.html',
+  styleUrl: './busca-filme.component.css'
+})
+export class BuscaFilmeComponent {
+
+}
